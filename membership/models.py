55fa72a965fa3ceabe4ff2a7dc03cb9fa7628497
Onah Jonah <onahjonah@gmail.com>
2024-01-20 02:40:43 +0100

    changes to model

--- membership/models.py
+++ membership/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import timezone
+from django.core.exceptions import ValidationError
 
 class User(models.Model):
     pass
@@ -10,9 +12,20 @@ class MembershipType(models.Model):
 
 
 class Subscription(models.Model):
+    def clean(self):
+        active_subs = Subscription.objects.filter(
+            user=self.user, 
+            end_date__gte=timezone.now()
+        ).exclude(id=self.id)
+        
+        if active_subs.exists():
+            raise ValidationError("You already have an active subscription")
+        
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     membership_type = models.ForeignKey(MembershipType, on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
     payment_status = models.CharField(max_length=20) # eg "paid" or "pending"
+    payment_id = models.CharField(max_length=255, blank=True)
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
     
